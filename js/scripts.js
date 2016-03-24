d1ecfe15b71e1d66237c0a6e4a2d2987fdb645a2
Robert Bunch <rob@digitalcrafts.com>
2016-03-24 09:09:14 -0400

    include typeahead - basic

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,59 @@ $(document).ready(function(){
 			newHTML += '<div class="col-sm-3">';
 			newHTML += '<img src="' + currentPoster + '">';
 			newHTML += '</div>';
-			console.log(currentPoster);
+			// console.log(currentPoster);
 		}
 		$('#poster-grid').html(newHTML);
 	});
 
+	$('#movie-form').submit(function(event){
+		var userSearch = $('.typeahead').val();
+		console.log(userSearch);
+		userSearch = $('#searchText').val();
+		console.log(userSearch);
+		// var typeSearch = ($('#movie-form .typeahead').typeahead('val'));
+		// console.log($('#movie-form .typeahead').typeahead('val'));
+		// console.log (typeSearch);
+		event.preventDefault();
+	});
+
+});
+
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var actors = [
+	'Brad Pitt',
+	'Michael Douglas',
+	'Al Pacino'
+];
+
+$('#movie-form .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'actors',
+  source: substringMatcher(actors)
 });
